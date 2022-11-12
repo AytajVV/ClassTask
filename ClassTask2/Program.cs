e24eebc7704e2c5cf6f611715f62fe090df4ee4f
Veyisli Aytaj <AytajVV@code.local>
2022-11-12 19:09:46 +0400

    Reverse String without string method

---
+++ ClassTask2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("hello"));
+        }
+       
+
+
+        static string Reverse(string name)
+        { 
+            string result = "";
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                result += name[i];
+            }
+            return result;
+        }
+    }
+}
