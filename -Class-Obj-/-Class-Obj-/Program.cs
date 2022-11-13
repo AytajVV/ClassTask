984f89f70e5f9692a3e77ca146724d45500bef78
AytajVV <115929516+AytajVV@users.noreply.github.com>
2022-11-13 19:15:51 +0400

    Add files via upload
    Class Object Constructor task

---
+++ -Class-Obj-/-Class-Obj-/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _Class_Obj_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form user = new Form("Aytac", "Veyisli", "1234567890", "Programming", "nthng");
+            user.GetInfo();
+            
+        }
+    }
+    class Form
+    {
+        public string Name;
+        public string Surname;
+        public string Phone;
+        public string Lessons;
+        public string Note;
+
+
+        public Form()
+        {
+
+        }
+
+      
+        public Form(string name, string surname, string phone, string lessons, string note)
+        {
+            Name = name;
+            Surname = surname;
+            Phone = phone;
+            Lessons = lessons;
+            Note = note;
+
+        }
+        public  void GetInfo()
+        {
+            Console.WriteLine( $"{Name} {Surname} {Phone} {Lessons} {Note} ");
+        }
+
+        
+
+        
+    }
+}
